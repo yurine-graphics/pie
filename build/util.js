c23c2ffdcf640318a709ea933fe1d965c836de42
army8735 <army8735@qq.com>
2015-12-22 15:25:24 +0800

    save

--- build/util.js
+++ build/util.js
@@ -6,6 +6,88 @@ function isType(type) {
   }
 }
 
+function find(context, s, maxWidth, i1, i2, w) {
+  if(i1 == i2 - 1) {
+    return i1;
+  }
+  w = w || context.measureText(s).width;
+  if(w == maxWidth) {
+    return i2;
+  }
+  else if(w > maxWidth) {
+    var i = i2 - (Math.floor((i2 - i1) / 2));
+    var s2 = s.slice(0, i);
+    var w2 = context.measureText(s2).width;
+    if(w2 > maxWidth) {
+      return find(context, s2, maxWidth, 0, i, w2);
+    }
+    else {
+      return find(context, s, maxWidth, i, i2,  w);
+    }
+  }
+  else {
+    var i = i1 + (Math.ceil((i2 - i1) / 2));
+    return find(context, s.slice(0, i), maxWidth, i, i2);
+  }
+}
+
 exports["default"]={
-  isString: isType('String')
+  isString: isType('String'),
+  calHeight: function(context, s, maxWidth) {
+    var arr = [];
+    var len = s.length;
+    var i = find(context, s, maxWidth, 0, len);
+    arr.push(s.slice(0, i));
+    while(i < len - 1) {
+      s = s.slice(i);
+      len = s.length;
+      i = find(context, s, maxWidth, 0, len);
+      arr.push(s.slice(0, i));
+    }
+    return arr;
+  },
+  calFont: function(s) {
+    var fontStyle = 'normal';
+    var fontVariant = 'normal';
+    var fontFamily = 'Arial';
+    var fontWeight = 'normal';
+    var fontSize = 12;
+    var lineHeight = '1.5';
+    if(/^[a-z]/i.test(s)) {
+      fontStyle = /^[a-z]+/.exec(s)[0];
+      s = s.replace(/^[a-z]+\s+/i, '');
+    }
+    if(/^[a-z]/i.test(s)) {
+      fontVariant = /^[a-z]+/.exec(s)[0];
+      s = s.replace(/^[a-z]+\s+/i, '');
+    }
+    if(/^[a-z]/i.test(s)) {
+      fontWeight = /^[a-z]+/.exec(s)[0];
+      s = s.replace(/^[a-z]+\s+/i, '');
+    }
+    if(/^\d/.test(s)) {
+      fontSize = parseInt(s);
+      s = s.replace(/^[\d.a-z]+/i, '');
+    }
+    if(/^\//.test(s)) {
+      s = s.slice(1);
+      lineHeight = /^[\d.a-z]+/i.exec(s)[0];
+      s = s.replace(/^[\d.a-z]+\s+/i, '');
+      if(/[a-z]$/i.test(lineHeight)) {
+        lineHeight = parseInt(lineHeight);
+      }
+      else {
+        lineHeight *= fontSize;
+      }
+    }
+    fontFamily = s;
+    return {
+      fontStyle:fontStyle,
+      fontVariant:fontVariant,
+      fontFamily:fontFamily,
+      fontWeight:fontWeight,
+      fontSize:fontSize,
+      lineHeight:lineHeight
+    };
+  }
 };
--- web/Radio.js
+++ web/Radio.js
@@ -2,6 +2,15 @@ define(function(require, exports, module){var util=function(){var _0=require('./
 
 var colors = ['4A90E2', 'C374DE', 'F36342', 'F3A642', '93C93F', '50E3C2'];
 
+function getColor(option, i) {
+  var idx = i % colors.length;
+  var color = option.colors[idx] || colors[idx];
+  if(color.indexOf(0) != '#' && color.charAt(0) != 'r') {
+    color = '#' + color;
+  }
+  return color;
+}
+
 
   function Radio(dom, data, option) {
     this.dom = util.isString(dom) ? document.querySelector(dom) : dom;
@@ -18,7 +27,30 @@ var colors = ['4A90E2', 'C374DE', 'F36342', 'F3A642', '93C93F', '50E3C2'];
     var context = this.dom.getContext('2d');
     var width = this.option.width || 400;
     var height = this.option.height || 400;
-    var min = Math.min(width, height);
+    var padding = this.option.padding || [10, 10, 10, 10];
+    if(Array.isArray(padding)) {
+      switch(padding.length) {
+        case 0:
+          padding = [10, 10, 10, 10];
+          break;
+        case 1:
+          padding[3] = padding[2] = padding[1] = padding[0];
+          break;
+        case 2:
+          padding[3] = padding[1];
+          padding[2] = padding[0];
+          break;
+        case 3:
+          padding[3] = padding[1];
+          break;
+      }
+    }
+    else {
+      padding = [padding, padding, padding, padding];
+    }
+    var paddingX = padding[1] + padding[3];
+    var paddingY = padding[0] + padding[2];
+    var min = Math.min(width - paddingX, height - paddingY);
     var lineWidth = this.option.lineWidth || 20;
     if(lineWidth < 4) {
       lineWidth = 4;
@@ -26,36 +58,39 @@ var colors = ['4A90E2', 'C374DE', 'F36342', 'F3A642', '93C93F', '50E3C2'];
     else if(lineWidth > min >> 1) {
       lineWidth = min >> 1;
     }
-    var padding = this.option.padding || [10, 10, 10, 10];
-    var paddingX = padding[1] + padding[3];
-    var paddingY = padding[0] + padding[2];
-    var size = String(this.option.size || 0.5);
+    var size = String(this.option.size || 1);
     if(/%$/.test(size)) {
       size = parseFloat(size) * 0.01;
     }
     else {
       size = parseFloat(size);
     }
-    if(size > 0.5) {
-      size = 0.5;
+    if(size > 1) {
+      size = 1;
     }
     else if(size < 0.2) {
       size = 0.2;
     }
-    var radio = (min * size - lineWidth - Math.max(paddingX, paddingY)) >> 1;
+    var radio = (min * size - lineWidth) >> 1;
 
-    this.renderBg(context, min, radio, lineWidth);
-    this.renderFg(context, min, radio, lineWidth);
+    this.renderBg(context, min, radio, lineWidth, padding);
+    this.renderFg(context, min, radio, lineWidth, padding);
+    this.renderTxt(context, radio, lineWidth, padding, width, height);
   }
-  Radio.prototype.renderBg = function(context, min, radio, lineWidth) {
+  Radio.prototype.renderBg = function(context, min, radio, lineWidth, padding) {
+    var gr = context.createRadialGradient(padding[3] + radio, min >> 1, radio - (lineWidth >> 1), padding[3] + radio, min >> 1, radio + (lineWidth >> 1));
+    gr.addColorStop(0, 'rgba(0,0,0,0)');
+    gr.addColorStop(0.2, 'rgba(0,0,0,0.1)');
+    gr.addColorStop(0.8, 'rgba(0,0,0,0.1)');
+    gr.addColorStop(1, 'rgba(0,0,0,0)');
     context.beginPath();
-    context.strokeStyle = 'rgba(0,0,0,0.1)';
+    context.strokeStyle = gr;
     context.lineWidth = lineWidth;
-    context.arc(min >> 1, min >> 1, radio, 0, (Math.PI/180)*360);
+    context.arc(padding[3] + radio, min >> 1, radio, 0, (Math.PI/180)*360);
     context.stroke();
     context.closePath();
   }
-  Radio.prototype.renderFg = function(context, min, radio, lineWidth) {
+  Radio.prototype.renderFg = function(context, min, radio, lineWidth, padding) {
     var self = this;
     var sum = 0;
     self.data.forEach(function(item) {
@@ -63,25 +98,113 @@ var colors = ['4A90E2', 'C374DE', 'F36342', 'F3A642', '93C93F', '50E3C2'];
     });
     var count = 0;
     self.data.forEach(function(item, i) {
-      self.renderItem(item, i, context, min, radio, lineWidth, count, sum);
+      self.renderItem(item, i, context, min, radio, lineWidth, padding, count, sum);
       count += parseFloat(item[1]);
     });
   }
-  Radio.prototype.renderItem = function(item, i, context, min, radio, lineWidth, count, sum) {
-    var idx = i % colors.length;
-    var color = this.option.colors[idx] || colors[idx];
-    if(color.indexOf(0) != '#') {
-      color = '#' + color;
-    }
+  Radio.prototype.renderItem = function(item, i, context, min, radio, lineWidth, padding, count, sum) {
+    var color = getColor(this.option, i);
     context.beginPath();
     context.strokeStyle = color;
     context.lineWidth = lineWidth >> 1;
     var start = (Math.PI/180)*360*count/sum;
     var num = parseFloat(item[1]);
     var end = start + (Math.PI/180)*360*num/sum;
-    context.arc(min >> 1, min >> 1, radio, start, end);
+    context.arc(padding[3] + radio, min >> 1, radio, start, end);
     context.stroke();
     context.closePath();
+  }
+  Radio.prototype.renderTxt = function(context, radio, lineWidth, padding, width, height) {
+    var lineHeight;var fontSize;var fontWeight;var fontFamily;var fontVariant;var fontStyle;var self = this;
+    var font = this.option.font || 'normal normal normal 12px/1.5 Arial';
+    !function(){var _1= util.calFont(font);fontStyle=_1["fontStyle"];fontVariant=_1["fontVariant"];fontFamily=_1["fontFamily"];fontWeight=_1["fontWeight"];fontSize=_1["fontSize"];lineHeight=_1["lineHeight"]}();
+
+
+    var color = this.option.color || '#000';
+    if(color.charAt(0) != '#' && color.charAt(0) != 'r') {
+      color = '#' + color;
+    }
+    context.textBaseline = 'top';
+
+    if(this.option.fontSize) {
+      fontSize = parseInt(this.option.fontSize) || 12;
+    }
+    fontSize = Math.max(fontSize, 12);
+    if(this.option.lineHeight) {
+      lineHeight = this.option.lineHeight;
+      if(util.isString(lineHeight)) {
+        if(/[a-z]$/i.test(lineHeight)) {
+          lineHeight = parseInt(lineHeight);
+        }
+        else {
+          lineHeight *= fontSize;
+        }
+      }
+      else {
+        lineHeight *= fontSize;
+      }
+    }
+    lineHeight = Math.max(lineHeight, fontSize);
+
+    font = fontStyle + ' ' + fontVariant + ' ' + fontWeight + ' ' + fontSize + 'px/' + lineHeight + 'px ' + fontFamily;
+    context.font = font;
+
+    var discRadio = parseInt(this.option.discRadio) || 5;
+    if(discRadio < 2) {
+      discRadio = 2;
+    }
+    else if(discRadio > (fontSize >> 1)) {
+      discRadio = fontSize >> 1;
+    }
+    var x = padding[3] + (radio << 1) + (lineWidth >> 1);
+    var maxWidth = width - padding[1] - x - (discRadio << 1) - 20;
+
+    var maxTextWidth = 0;
+    var totalHeight = 0;
+    var heights = [];
+    self.data.forEach(function(item) {
+      item[0] = item[0] || '';
+      var w = context.measureText(item[0]).width;
+      if(w > maxWidth) {
+        var arr = util.calHeight(context, item[0], maxWidth);
+        item[0] = arr;
+        totalHeight += heights.push(arr.length * lineHeight);
+        maxTextWidth = maxWidth;
+      }
+      else {
+        totalHeight += heights.push(lineHeight);
+        maxTextWidth = Math.max(w, maxTextWidth);
+      }
+    });
+
+    x += (maxWidth - maxTextWidth) >> 1;
+    var offset = (height - totalHeight) >> 1;
+
+    var count = 0;
+    self.data.forEach(function(item, i) {
+      self.renderTxtItem(item, i, context, x, offset, count, discRadio, color, fontSize, lineHeight, maxWidth);
+      count += heights[i];
+    });
+  }
+  Radio.prototype.renderTxtItem = function(item, i, context, x, offset, count, discRadio, txtColor, fontSize, lineHeight) {
+    var color = getColor(this.option, i);
+    context.fillStyle = color;
+    context.beginPath();
+    var y = count + ((lineHeight - ((lineHeight - fontSize) >> 1)) >> 1) + (offset >> 1);
+    context.arc(x + 10, y, discRadio, 0, (Math.PI/180)*360);
+    context.fill();
+    context.closePath();
+
+    context.fillStyle = txtColor;
+    var txt = item[0] || i;
+    if(Array.isArray(txt)) {
+      txt.forEach(function(t, j) {
+        context.fillText(t, x + 20, count + (offset >> 1) + j * lineHeight);
+      });
+    }
+    else {
+      context.fillText(txt, x + 20, count + (offset >> 1));
+    }
   }
 
 
