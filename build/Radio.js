d512ae040aa786cf04a6f1cc8286bb1c8265c123
army8735 <army8735@qq.com>
2016-11-09 17:21:01 +0800

    自动计算宽高

--- build/Radio.js
+++ build/Radio.js
@@ -30,8 +30,8 @@ function getColor(option, i) {
 
   Radio.prototype.render = function() {
     var y;var x;var context = this.dom.getContext('2d');
-    var width = this.option.width || 300;
-    var height = this.option.height || 150;
+    var width = this.option.width || this.dom.getAttribute('width') || parseInt(window.getComputedStyle(this.dom, null).getPropertyValue('width')) || 300;
+    var height = this.option.height || this.dom.getAttribute('height') || parseInt(window.getComputedStyle(this.dom, null).getPropertyValue('height')) || 150;
     var padding = this.option.padding === undefined ? [10, 10, 10, 10] : this.option.padding;
     if(Array.isArray(padding)) {
       switch(padding.length) {
